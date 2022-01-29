dbe22defd798b9afa85e001a6117808b49d22dc9
Oksana Staeglazova <oksanik.med@gmail.com>
2022-01-30 02:09:42 +0300

    task import-export done

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin()],
+};
